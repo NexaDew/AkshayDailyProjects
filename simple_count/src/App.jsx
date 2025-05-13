bcbe8e43eb70411d98e37b57a942bae792731394
AkshayReddy60 <158162320+AkshayReddy60@users.noreply.github.com>
2025-05-13 10:00:31 +0530

    Counter (#2)
    * simple count
    
    * simple counter application

--- simple_count/src/App.jsx
+++ simple_count/src/App.jsx
@@ -1,28 +1,25 @@
 
-import React,{ useState } from 'react';
-import './App.css'
-
+import React, { useState } from 'react';
+import './App.css';
 
 function App() {
+  const [count, setCount] = useState(0);
 
-  const [count,setCout]=useState(0);
-
-  const increment=()=>setCout(count+1);
-  const decrement=()=>setCout(count-1);
-  const reset=()=>setCout(0);
+  const increment = () => setCount(count + 1);
+  const decrement = () => setCount(count - 1);
+  const reset = () => setCount(0);
 
   return (
-    
-     <div className='container'>
+    <div className='container'>
       <h2>Simple Counter</h2>
       <h4>Count: {count}</h4>
       <div className="button">
-       <button onClick={increment}>Increment</button>
-       <button onClick={decrement}>Decrement</button>
-       <button onClick={reset}>Reset</button>
+        <button onClick={increment}>Increment</button>
+        <button onClick={decrement}>Decrement</button>
+        <button onClick={reset}>Reset</button>
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
